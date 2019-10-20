1b0bf828251be1c2c72f115725bdae8dc3db7d67
hejianbo <hejanbo@qq.com>
2019-10-20 09:58:35 +0800

    define list, stack, queue interface

--- algorithm/src/main/java/me/jianbo/ds/chapter02/MyArrayQueue.java
+++ algorithm/src/main/java/me/jianbo/ds/chapter02/MyArrayQueue.java
@@ -2,4 +2,34 @@ package me.jianbo.ds.chapter02;
 
 public class MyArrayQueue<E> implements MyQueue<E> {
 
+	@Override
+	public MyQueue<E> createQueue(int maxSize) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Boolean isFull() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Boolean isEmpty() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void add(E item) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public E delete() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
 }
